a3a33bd82809392c53476923b649e7dc9d8c638e
nesmabelkhodja <nb2253@nyu.edu>
2017-11-18 01:56:30 -0500

    functionality/ determining winner

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -10,6 +10,8 @@ function main(){
  	let stnrdDeck = createDeck();
  	let playerTotal = 0;
  	let cpuTotal = 0;
+ 	let count = 1;
+ 	let cpuCount = 1;
 	
 	document.querySelector(".playBtn").addEventListener("click", function(evt){
 		evt.preventDefault();
@@ -45,28 +47,35 @@ function main(){
 		deck = deck.concat(stnrdDeck);
 
 		//adding starting cards
-		cpu.push(deck[0]);
-		cpu.push(deck[2]);
-		player.push(deck[1]);
-		player.push(deck[3]);
+		cpu.push(deck.shift());
+		player.push(deck.shift());
+		cpu.push(deck.shift());
+		player.push(deck.shift());
 
-		//adding starting cards to total
+		//adding starting cards to totallet temp = deck.shift();
 		playerTotal = player[0].num + player[1].num;
+		if (playerTotal > 21 && (player[0].value === 'A' || player[1].value === 'A')){
+				playerTotal-=10;
+			}
+
 		cpuTotal = cpu[0].num + cpu[1].num;
+		if (cpuTotal > 21 && (cpu[0].value === 'A' || cpu[1].value === 'A')){
+				cpuTotal-=10;
+			}
 
 
 		//display starting cards and totals
 		//display player total
 		let heading = document.createElement("h1");
-		let content = document.createTextNode("Computer Hand - Total: "+cpuTotal);
+		let content = document.createTextNode("Computer Hand - Total: ?");
 		heading.appendChild(content);
 
 		let currentDiv = document.querySelector(".game");
 		document.body.appendChild(heading, currentDiv);
 
 		//adding total
-		newDiv = document.createElement("p");
-		content = document.createTextNode(cpu[0].value+" "+cpu[0].suit);
+		newDiv = document.createElement("span");
+		content = document.createTextNode(" ");
 		newDiv.appendChild(content);
 
 		currentDiv = document.querySelector(".game");
@@ -103,25 +112,82 @@ function main(){
 		document.body.appendChild(newDiv, currentDiv);
 
 		//creating buttons
-		let btn = document.createElement("BUTTON");
+		let hit = document.createElement("BUTTON");
 		content = document.createTextNode("Hit");
-		btn.appendChild(content);
+		hit.className = "hit"; 
+		hit.appendChild(content);
 
 		currentDiv = document.querySelector(".game");
-		document.body.appendChild(btn, currentDiv);
+		document.body.appendChild(hit, currentDiv);
 
-		btn = document.createElement("BUTTON");
+		//stand
+		let stand = document.createElement("BUTTON");
 		content = document.createTextNode("Stand");
-		btn.appendChild(content);
+		stand.className = "stand";
+		stand.appendChild(content);
 
 		currentDiv = document.querySelector(".game");
-		document.body.appendChild(btn, currentDiv);
+		document.body.appendChild(stand, currentDiv);
+
+		//hit
+		hit.addEventListener("click", function(evt){
+			let temp = deck.shift();
+			player.push(temp);
+			playerTotal += parseInt(temp.num);
+			if (playerTotal > 21 && (player[player.length-1].value === 'A')){
+				playerTotal-=10;
+			}
+
+			//new card
+			newDiv = document.createElement("p");
+			content = document.createTextNode(player[player.length-1].value+" "+player[player.length-1].suit);
+			newDiv.appendChild(content);
+
+			currentDiv = document.querySelector(".game");
+			document.body.appendChild(newDiv, currentDiv);
+
+			//check if there is a winner
+			if (playerTotal > 21){
+				renderResult(player, cpu, playerTotal, cpuTotal, "You lose!!! :(")
+			}
+
+		});
+
+		//stand
+		stand.addEventListener("click", function(evt){
+			const act = Math.floor(Math.random() * 2);
+			if (act === 1){
+			let temp = deck.shift();
+			cpu.push(temp);
+			cpuTotal += parseInt(temp.num);
+			if (cpuTotal > 21 && (cpu[cpu.length-1].value === 'A')){
+				cpuTotal-=10;
+			}
+
+			if (cpuTotal>21)
+			{
+				renderResult(player, cpu, playerTotal, cpuTotal, "You win!!! :)");
+			}
+		}
+		if (act === 2){
+			if (playerTotal > cpuTotal){
+				renderResult(player, cpu, playerTotal, cpuTotal, "You win!!! :)");
+			}
+			else if (cpuTotal > playerTotal){
+				renderResult(player, cpu, playerTotal, cpuTotal, "You lose!!! :(");
+			}
+			else if (playerTotal === cpuTotal)
+			{
+				renderResult(player, cpu, playerTotal, cpuTotal, "It's a tie!");
+			}
+		}
+
+		});
 
 		//create and apply the appropriate CSS classes to get rid of the form (do this with styles, there's no need to remove the element)
 		form.classList.toggle('hidden');
 		
 		});
-
 }
 
 
@@ -131,7 +197,10 @@ function createDeck(){
 	let suit = '';
 	let card = {};
 	for(num=1; num<=12; num++){
-		if (num === 1){
+		if (num<10){
+			number = num;
+		}
+		else if (num === 1){
 			number = 'A';
 			num = 11;
 		}
@@ -144,9 +213,6 @@ function createDeck(){
 		else if (num === 12) {
 			number = 'K';
 		}
-		else
-			number = num;
-
 		for(s=0; s<4; s++){
 			if (s===0){
 				suit = '♠';
@@ -174,6 +240,56 @@ function createDeck(){
     }
 
 	return deck;
+}
+
+function renderResult(player, cpu, playerTotal, cpuTotal, message){
+	document.body.innerHTML = '';
+		//display player total
+		let heading = document.createElement("h1");
+		let content = document.createTextNode("Computer Hand - Total: "+cpuTotal);
+		heading.appendChild(content);
+
+		let currentDiv = document.querySelector(".game");
+		document.body.appendChild(heading, currentDiv);
+
+		let newDiv = document.createElement("p");
+
+		//adding cpu cards
+		for (i=0; i<cpu.length; i++){
+			newDiv = document.createElement("p");
+			content = document.createTextNode(cpu[i].value+" "+cpu[i].suit);
+			newDiv.appendChild(content);
+
+			currentDiv = document.querySelector(".game");
+			document.body.appendChild(newDiv, currentDiv);
+		}
+
+		//display player total
+		heading = document.createElement("h1");
+		content = document.createTextNode("Player Hand - Total: "+playerTotal);
+		heading.appendChild(content);
+
+		currentDiv = document.querySelector(".game");
+		document.body.appendChild(heading, currentDiv);
+
+		//player cards
+			for (i=0; i<player.length; i++){
+			newDiv = document.createElement("p");
+			content = document.createTextNode(player[i].value+" "+player[i].suit);
+			newDiv.appendChild(content);
+
+			currentDiv = document.querySelector(".game");
+			document.body.appendChild(newDiv, currentDiv);
+		}
+
+		heading = document.createElement("h1");
+				content = document.createTextNode(message);
+				heading.appendChild(content);
+
+				currentDiv = document.querySelector(".game");
+				document.body.appendChild(heading, currentDiv);
+
+
 }
 
 document.addEventListener('DOMContentLoaded', main);
